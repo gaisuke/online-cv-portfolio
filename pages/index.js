1622d3704a04496dc83eaade6a8819d4c08417a0
Dani Munif <danimunif@cs.unej.ac.id>
2021-02-05 14:36:07 +0700

    crete development branch

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ export default function Home() {
     // </div>
     <>
       <h1 className="text-6xl text-center font-black">sadhkasjdh</h1> <br/>
-      <h1 className="text-center text-3xl font-black py-24">TESTdddING!!!!</h1>
+      <h1 className="text-center text-3xl font-black py-24">TESTdddINg!!!!</h1>
     </>
   )
 }
