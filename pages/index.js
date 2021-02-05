198cda7ee34e94fe9b249621528f6215c393c27a
Dani Munif <danimunif@cs.unej.ac.id>
2021-02-05 14:33:51 +0700

    create coming soon branch

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,73 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create "test" Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    // <div className={styles.container}>
+    //   <Head>
+    //     <title>Create "test" Next App</title>
+    //     <link rel="icon" href="/favicon.ico" />
+    //   </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    //   <main className={styles.main}>
+    //     <h1 className={styles.title}>
+    //       Welcome to <a href="https://nextjs.org">Next.js!</a>
+    //     </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    //     <p className={styles.description}>
+    //       Get started by editing{' '}
+    //       <code className={styles.code}>pages/index.js</code>
+    //     </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    //     <div className={styles.grid}>
+    //       <a href="https://nextjs.org/docs" className={styles.card}>
+    //         <h3>Documentation &rarr;</h3>
+    //         <p>Find in-depth information about Next.js features and API.</p>
+    //       </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    //       <a href="https://nextjs.org/learn" className={styles.card}>
+    //         <h3>Learn &rarr;</h3>
+    //         <p>Learn about Next.js in an interactive course with quizzes!</p>
+    //       </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+    //       <a
+    //         href="https://github.com/vercel/next.js/tree/master/examples"
+    //         className={styles.card}
+    //       >
+    //         <h3>Examples &rarr;</h3>
+    //         <p>Discover and deploy boilerplate example Next.js projects.</p>
+    //       </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+    //       <a
+    //         href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    //         className={styles.card}
+    //       >
+    //         <h3>Deploy &rarr;</h3>
+    //         <p>
+    //           Instantly deploy your Next.js site to a public URL with Vercel.
+    //         </p>
+    //       </a>
+    //     </div>
+    //     <div className="text-center w-full bg-blue-500 rounded-lg py-24">
+    //       <h1 className="text-white font-black text-2xl">COBA ELEMEN</h1>
+    //       <br/>
+    //     </div>
+    //   </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    //   <footer className={styles.footer}>
+    //     <a
+    //       href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Powered by{' '}
+    //       <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+    //     </a>
+    //   </footer>
+    // </div>
+    <>
+      <h1 className="text-6xl text-center font-black">sadhkasjdh</h1> <br/>
+      <h1 className="text-center text-3xl font-black py-24">TESTdddING!!!!</h1>
+    </>
   )
 }
